3a00c8afd2cb2c05f788eb59585bd921d5e1f864
Kayla Toll <pinksmorez@gmail.com>
2025-06-03 16:45:45 -0700

    changed 3d to unsigned ints also

--- find_just_one/find_just_one.cpp
+++ find_just_one/find_just_one.cpp
@@ -20,12 +20,12 @@ int factorial(int d) {
 
 // 3d ----------------------------------------------------------------------------------------------
 // path making
-void path_column_maker_3d(int group, int old_maximum, int addition, int *** previous_path_column, int *** current_path_column) {
-   int num_options;
+void path_column_maker_3d(int group, int old_maximum, int addition, unsigned int *** previous_path_column, unsigned int *** current_path_column) {
+   unsigned int num_options;
    for (int i = 0; i < old_maximum + 1; i++) {
       if (previous_path_column[i]) {
          if (not current_path_column[i + addition]) {
-            current_path_column[i + addition] = new int * [6];
+            current_path_column[i + addition] = new unsigned int * [6];
             for (int j = 0; j < 6; j++) {
                current_path_column[i + addition][j] = NULL;
             }
@@ -36,42 +36,42 @@ void path_column_maker_3d(int group, int old_maximum, int addition, int *** prev
                num_options += previous_path_column[i][j][1];
             }
          }
-         current_path_column[i + addition][group] = new int [2];
+         current_path_column[i + addition][group] = new unsigned int [2];
          current_path_column[i + addition][group][0] = i;
          current_path_column[i + addition][group][1] = num_options;
       }
    }
 }
 
-void path_maker_3d(int n, int **** paths) {
-   paths[0] = new int ** [n + 1];
+void path_maker_3d(int n, unsigned int **** paths) {
+   paths[0] = new unsigned int ** [n + 1];
    for (int i = 0; i < n + 1; i++) {
       paths[0][i] = NULL;
    }
-   paths[0][0] = new int * [6];
-   paths[0][n - 1] = new int * [6];
-   paths[0][n] = new int * [6];
+   paths[0][0] = new unsigned int * [6];
+   paths[0][n - 1] = new unsigned int * [6];
+   paths[0][n] = new unsigned int * [6];
    for (int i = 0; i < 6; i++) {
       paths[0][0][i] = NULL;
       paths[0][n - 1][i] = NULL;
       paths[0][n][i] = NULL;
    }
-   paths[0][n][0] = new int [2];
+   paths[0][n][0] = new unsigned int [2];
    paths[0][n][0][0] = 0;
    paths[0][n][0][1] = 1;
-   paths[0][0][1] = new int [2];
+   paths[0][0][1] = new unsigned int [2];
    paths[0][0][1][0] = 0;
    paths[0][0][1][1] = 1;
-   paths[0][n - 1][2] = new int [2];
+   paths[0][n - 1][2] = new unsigned int [2];
    paths[0][n - 1][2][0] = 0;
    paths[0][n - 1][2][1] = 1;
-   paths[0][n - 1][3] = new int [2];
+   paths[0][n - 1][3] = new unsigned int [2];
    paths[0][n - 1][3][0] = 0;
    paths[0][n - 1][3][1] = 1;
-   paths[0][0][4] = new int [2];
+   paths[0][0][4] = new unsigned int [2];
    paths[0][0][4][0] = 0;
    paths[0][0][4][1] = 1;
-   paths[0][0][5] = new int [2];
+   paths[0][0][5] = new unsigned int [2];
    paths[0][0][5][0] = 0;
    paths[0][0][5][1] = 1;
    int maximum = n;
@@ -79,7 +79,7 @@ void path_maker_3d(int n, int **** paths) {
    for (int i = 0; i < n - 1; i++) {
       old_maximum = maximum;
       maximum += (i + 2) * (n - i - 1);
-      paths[i + 1] = new int ** [maximum + 1];
+      paths[i + 1] = new unsigned int ** [maximum + 1];
       for (int j = 0; j < maximum + 1; j++) {
          paths[i + 1][j] = NULL;
       }
@@ -94,7 +94,7 @@ void path_maker_3d(int n, int **** paths) {
 
 
 // path printing
-void print_path_column_3d(int n, int column, int **** paths) {
+void print_path_column_3d(int n, int column, unsigned int **** paths) {
    cout << "column: ";
    cout << column;
    cout << "\n";
@@ -128,11 +128,14 @@ void print_path_column_3d(int n, int column, int **** paths) {
 
 
 // path searching
-vector<string> path_finder_recursive_3d(int n, int shares[], int translator[6][6], int w, int **** paths, int depth, string so_far) {
+vector<string> path_finder_recursive_3d(int n, int shares[], int translator[6][6], int w, unsigned int **** paths, int depth, string so_far) {
+   cout << "depth ";
+   cout << depth;
+   cout << "\n";
    vector<string> solutions = {};
    int maximum;
    int likeliests[w][6] = {{-1}};
-   int likeliest_quantity[w] = {-1};
+   long likeliest_quantity[w] = {-1};
    string likeliest_group[w] = {""};
    if (depth == 0) {
       maximum = 1;
@@ -140,31 +143,42 @@ vector<string> path_finder_recursive_3d(int n, int shares[], int translator[6][6
       maximum = 6;
    }
    for (int i = 0; i < maximum; i++) {
+      cout << "group " + get_group_3d[i] + "\n";
       if (paths[n - depth - 1][shares[0]][i]) {
          bool good = true;
          int next_shares[6] = {-1, -1, -1, -1, -1, -1};
-         int combined_path_options = paths[n - depth - 1][shares[0]][i][1];
+         long combined_path_options = paths[n - depth - 1][shares[0]][i][1];
          next_shares[0] = paths[n - depth - 1][shares[0]][i][0];
+         //cout << paths[n - depth - 1][shares[0]][translator[0][i]][1];
+         //cout << "\n";
          for (int j = 1; j < 6; j++) {
             if (paths[n - depth - 1][shares[j]][translator[j][i]]) {
                next_shares[j] = paths[n - depth - 1][shares[j]][translator[j][i]][0];
                combined_path_options += paths[n - depth - 1][shares[j]][translator[j][i]][1];
+               //cout << paths[n - depth - 1][shares[j]][translator[j][i]][1];
+               //cout << "\n";
             } else {
                good = false;
             }
          }
+         cout << combined_path_options;
+         cout << "\n";
          if (good) {
             int insertion_point = -1;
             for (int k = 0; k < w; k++) {
-               if (combined_path_options > likeliest_quantity[k]) {
+               if (combined_path_options >= likeliest_quantity[k]) {
                   if (insertion_point == -1) {
                      insertion_point = k;
                   }
+               } else if (combined_path_options < -1) {
+                  cout << "oh no!\n";
+                  cout << combined_path_options;
+                  cout << "\n";
                }
             }
             if (insertion_point > -1) {
                for (int k = w - 1; k > insertion_point; k--) {
-                  for (int m = 0; m < 6; m ++) {
+                  for (int m = 0; m < 6; m++) {
                      likeliests[k][m] = likeliests[k - 1][m];
                   }
                   likeliest_quantity[k] = likeliest_quantity[k - 1];
@@ -179,8 +193,14 @@ vector<string> path_finder_recursive_3d(int n, int shares[], int translator[6][6
          }
       }
    }
+   cout << "likeliest_quantity[0] ";
+   cout << likeliest_quantity[0];
+   cout << "\n";
    for (int k = 0; k < w; k++) {
       if (likeliest_quantity[k] > 0) {
+         cout << "likeliest_group[k] ";
+         cout << likeliest_group[k];
+         cout << "\n";
          if (depth == n - 1) {
             solutions.push_back(so_far + likeliest_group[k]);
          } else {
@@ -192,7 +212,7 @@ vector<string> path_finder_recursive_3d(int n, int shares[], int translator[6][6
    return solutions; 
 }
 
-vector<string> path_finder_3d(int n, int share, int w, int **** paths) {
+vector<string> path_finder_3d(int n, int share, int w, unsigned int **** paths) {
    int shares[6] = {share, share, share, share, share, share};
    int translator[6][6] = {{0, 1, 2, 3, 4, 5}, \
                            {1, 0, 4, 5, 2, 3}, \
@@ -592,12 +612,12 @@ vector<string> path_finder_4d(int n, int share, int w, unsigned int ***** paths)
 
 
 int main() {
-   int d = 4;
-   int n = 12;
-   int w = 24; // search width
+   int d = 3;
+   int n = 24;
+   int w = 1; // search width
    int share = pow(n, d) / factorial(d);
    if (d == 3) {
-      int **** paths = new int *** [n];
+      unsigned int **** paths = new unsigned int *** [n];
       path_maker_3d(n, paths);
       vector<string> solutions = path_finder_3d(n, share, w, paths);
       for (string solution: solutions) {
